a16846d26b85402f8f9310a6f6088c0cd874a9f8
Piotr Zalewa <piotr@zalewa.info>
2012-10-08 14:37:53 +0200

    setup.py fixed

--- setup.py
+++ setup.py
@@ -2,8 +2,23 @@ from setuptools import setup
 
 setup(
     name='Marketplace',
-    version='0.1a',
-    packages=['marketplace',],
+    version='0.1.2a',
+    packages=['marketplace', ],
     license='Mozilla Public License (MPL 2.0)',
-    long_description=open('README.rst').read(),
+    author='Piotr Zalewa',
+    author_email='zalun@mozilla.com',
+    url='https://github.com/mozilla/Marketplace.Python',
+    long_description="""
+Marketplace Client Library
+
+Mozilla is building a Marketplace to bring personalized discovery,
+worldwide distribution, and easy payments to the largest platform
+for app development: the Web.
+
+This library helps to create Python based sites or apps to communicate
+with Marketplace.
+
+You may find the source code and collaborate your time and experience at
+https://github.com/mozilla/Marketplace.Python
+""",
     install_requires=['httplib2', 'oauth2', 'requests'])
